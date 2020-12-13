a3b601596b761f3fb628b69e2d571b76e7601d21
icweaver <iweaver@cfa.harvard.edu>
2020-12-13 15:31:01 -0500

    asdf

--- notebook.jl
+++ notebook.jl
@@ -16,16 +16,11 @@ end
 # ╔═╡ de8e285c-f93c-11ea-1571-a5a5dbc48e3c
 using PlutoUI, Plots, StatsPlots, CSV, CCDReduction, FITSIO, DataFrames, DataFramesMeta, Glob, AstroImages, HDF5, Dates, Printf, PyCall, RecursiveArrayTools, BenchmarkTools, Statistics, PaddedViews
 
-# ╔═╡ 7b37c734-3a70-11eb-006b-c75ac3f4ea6e
-begin
-	import DarkMode
-end
-
 # ╔═╡ cea3f932-f935-11ea-0eb3-4f47e65da5fa
-md"## $(@bind run_raw_stats CheckBox()) File stats"
+md"## $(@bind run_file_stats CheckBox()) File stats"
 
 # ╔═╡ 18536d9c-f922-11ea-02ed-5340b350c0c0
-if run_raw_stats
+if run_file_stats
 	df_raw_data = CCDReduction.fitscollection(
 		data_path,
 		abspath=false,
@@ -75,7 +70,7 @@ if run_raw_stats
 end
 
 # ╔═╡ f802dde4-f92a-11ea-2bf6-bd8d80f69a3a
-if run_raw_stats
+if run_file_stats
 	#by(df_raw_data, :OBJECT, nrow)
 	HTMLTable(head(@where(df_raw_data, occursin.("science", :OBJECT))))
 end
@@ -126,7 +121,6 @@ function plot_spectra!(p, data; key="Wavelength", label=nothing)
 	d = data[:, i+1, :]
 	extracted_spectra_med = median(d, dims=2)
 	σ = std(d, dims=2)
-	println(size(σ), size(extracted_spectra_med))
 	plot!(
 		p,
 		extracted_spectra_med,
@@ -349,8 +343,8 @@ end
 begin
 	theme(:dark)
 	default(
-		titlefont = "Helvetica",
-		guidefont = "Helvetica",
+		titlefont = "Lato",
+		guidefont = "Lato",
 		markerstrokewidth = 0,
 	)
 end
@@ -359,37 +353,36 @@ end
 plotly()
 
 # ╔═╡ Cell order:
-# ╠═7b37c734-3a70-11eb-006b-c75ac3f4ea6e
-# ╟─cea3f932-f935-11ea-0eb3-4f47e65da5fa
-# ╟─18536d9c-f922-11ea-02ed-5340b350c0c0
+# ╠═cea3f932-f935-11ea-0eb3-4f47e65da5fa
+# ╠═18536d9c-f922-11ea-02ed-5340b350c0c0
 # ╠═f802dde4-f92a-11ea-2bf6-bd8d80f69a3a
 # ╟─0110f78a-f945-11ea-004c-291cbfd9365c
 # ╠═362246e4-3a44-11eb-14b3-13a687b7f39b
 # ╠═65530184-f968-11ea-3f55-7d8a470384ed
 # ╟─6f8188e8-f98f-11ea-0bd4-bd30f24b976e
 # ╟─51666c34-39bd-11eb-0e2a-2371e207dce8
-# ╟─e7e183b0-fb4c-11ea-157d-c11e7dbb1a1c
-# ╟─d6251ef2-fb4c-11ea-1ce3-97a485de4b95
-# ╟─6d2b41fc-fb96-11ea-0ca4-1d62d94d25d7
+# ╠═e7e183b0-fb4c-11ea-157d-c11e7dbb1a1c
+# ╠═d6251ef2-fb4c-11ea-1ce3-97a485de4b95
+# ╠═6d2b41fc-fb96-11ea-0ca4-1d62d94d25d7
 # ╟─93b51aba-fba4-11ea-2181-a9f87f9348e4
 # ╟─61c9a1ea-3c1a-11eb-2623-ff87059cf057
-# ╠═6b0c5d92-3bf7-11eb-03eb-871c9ffbd2e8
-# ╟─9edfdb1e-fba4-11ea-2223-a5266f7ec2bb
+# ╟─6b0c5d92-3bf7-11eb-03eb-871c9ffbd2e8
+# ╠═9edfdb1e-fba4-11ea-2223-a5266f7ec2bb
 # ╟─25af8e98-fba8-11ea-2089-db810bb8778a
 # ╟─a8e8d9e2-3c1a-11eb-2bc3-bd1f05686d01
 # ╟─e6440db6-3c1a-11eb-14a4-81c7c7b23a80
-# ╟─dc2a31a2-3c1a-11eb-0627-e76f27095b61
-# ╟─07920dee-3c1e-11eb-2885-33fcf07a818f
+# ╠═dc2a31a2-3c1a-11eb-0627-e76f27095b61
+# ╠═07920dee-3c1e-11eb-2885-33fcf07a818f
 # ╟─767b67be-3714-11eb-2a8b-e13213320300
 # ╠═586f547e-3714-11eb-0fa1-cf1a1ebd5f50
 # ╠═fbaf9f98-3cb6-11eb-2689-05f6174fd509
 # ╠═72c7e8f4-3cc3-11eb-2f6a-07aff33cd8aa
 # ╟─8fbbec74-f970-11ea-34d8-174a293a4107
 # ╟─4494f844-f98b-11ea-330d-e3c18965972d
-# ╟─b38c37b6-f970-11ea-033e-6d215c8b87df
-# ╟─8041e34c-f971-11ea-0a9e-dd6415b3f33c
+# ╠═b38c37b6-f970-11ea-033e-6d215c8b87df
+# ╠═8041e34c-f971-11ea-0a9e-dd6415b3f33c
 # ╟─03cd4060-fab6-11ea-1298-ebbeccac9d09
 # ╟─7b2ef6f4-f9e3-11ea-22ed-0367935634be
-# ╠═6319a232-f8a3-11ea-0a58-7bf7af34ff4d
+# ╟─6319a232-f8a3-11ea-0a58-7bf7af34ff4d
 # ╟─1497392c-f96d-11ea-3e29-15d8b60f4559
 # ╠═de8e285c-f93c-11ea-1571-a5a5dbc48e3c
